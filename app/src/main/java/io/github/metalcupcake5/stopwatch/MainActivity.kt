7884e4a2bdaedabf93ce231fdcddca3fc5f0f2f0
metalcupcake5 <imcoolcom5@gmail.com>
2020-10-01 11:17:33 -0700

    Add stopwatch function, start, stop, reset

--- app/src/main/java/io/github/metalcupcake5/stopwatch/MainActivity.kt
+++ app/src/main/java/io/github/metalcupcake5/stopwatch/MainActivity.kt
@@ -2,12 +2,38 @@ package io.github.metalcupcake5.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_ma
+        var pausedTime = 0L
+
+        button_main_start.setOnClickListener {
+            Log.v(TAG, "chonometer: ${chronometer_main_timer.base}")
+            Log.v(TAG, "System: ${SystemClock.elapsedRealtime()}")
+            if (pausedTime > 0) chronometer_main_timer.base += SystemClock.elapsedRealtime() - pausedTime else chronometer_main_timer.base = SystemClock.elapsedRealtime()
+            chronometer_main_timer.start()
+        }
+
+        button_main_pause.setOnClickListener {
+            chronometer_main_timer.stop()
+            pausedTime = SystemClock.elapsedRealtime()
+        }
+
+        button_main_reset.setOnClickListener {
+            pausedTime = 0L
+            chronometer_main_timer.base = SystemClock.elapsedRealtime()
+            chronometer_main_timer.stop()
+        }
     }
+
+
 }
